b05e87513b8497b4f3053b68b9ef7f3968a6d0f6
nextpassword99 <inderscop@gmail.com>
2025-01-06 10:06:39 -0500

    chore: Modificar la ruta de los archivos

--- app/transcription/transcription_service.py
+++ app/transcription/transcription_service.py
@@ -1,6 +1,6 @@
 from app.audio_processing.audio_processor import AudioProcessor
 from app.audio_processing.file_manager import FileManager
-from app.whisper_transcriber import WhisperTranscriber
+from app.transcription.whisper_transcriber import WhisperTranscriber
 
 
 class TranscriptionService:
